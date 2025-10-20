f479a23f0a06a45c1a8319030e07d0e5a4af4f01
Máximo quiroga <123843504+MaxIIIDev@users.noreply.github.com>
2025-10-20 16:12:44 -0300

    Modelos
    Sin atributos

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+118ca810f6763844f300311a5a84301dfe4e732d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34315f15c05b651f109402b67af24e8d2101e511")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
