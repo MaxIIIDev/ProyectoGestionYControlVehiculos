00644b952421c75d02648e76184f63a20ab0c539
Máximo quiroga <123843504+MaxIIIDev@users.noreply.github.com>
2025-10-30 17:11:03 -0300

    Delete HelperFor.cs

--- Backend/Helpers/HelperFor.cs
+++
@@ -1,6 +0,0 @@
-
-
-public class HelperFor
-{
-    public static void ShowErrorMessageInConsole(string className, string methodName, string message) => Console.WriteLine($"Error en la clase: {className} , en el metodo: {methodName} , con el siguiente error: {message}");
-}
