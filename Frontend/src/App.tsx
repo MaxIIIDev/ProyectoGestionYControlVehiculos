dc3d49e1914c1d28662b85aea32865cfb0088bc4
AnddyPest <mauripestchanker@gmail.com>
2025-11-03 15:16:50 -0300

    ahi ta

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,16 +3,15 @@ import "./App.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router } from "react-router-dom";
-import { Container, Form, Row } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 
 import TopNavBar from "./Components/TopNavBar";
 import SideNavBar from "./Components/SideNavBar";
 import RoutesComponent from "./Components/RoutesComponent";
-import FormInput from "./Components/FormInput";
 
 function App() {
   return (
-    /*<>
+    <>
       <Router>
         <TopNavBar />
         <Container fluid>
@@ -22,18 +21,6 @@ function App() {
           </Row>
         </Container>
       </Router>
-    </>*/
-    <>
-      <Form>
-        <FormInput placeholder="Nombre" type="text" name="name" id="name" />
-        <FormInput placeholder="Email" type="email" name="email" id="email" />
-        <FormInput
-          placeholder="Contraseña"
-          type="password"
-          name="password"
-          id="password"
-        />
-      </Form>
     </>
   );
 }
--- Frontend/src/Components/Form.tsx
+++ Frontend/src/Components/Form.tsx
@@ -2,7 +2,13 @@ import type { ReactNode } from "react";
 
 interface FormProps {
   children?: ReactNode;
+  name: string;
+  method: string;
 }
-export default function Form({ children }: FormProps) {
-  return <form>{children}</form>;
+export default function Form({ children, name, method }: FormProps) {
+  return (
+    <form name={name} method={method}>
+      {children}
+    </form>
+  );
 }
