9b82cb4738eccfc1310e4d2f7984f05f582c18d8
AnddyPest <mauripestchanker@gmail.com>
2025-11-07 15:44:22 -0300

    prueba de centralizacion de funciones

---
+++ Frontend/src/Components/Functions/FormFunctions.tsx
@@ -0,0 +1,50 @@
+export const onSubmit = (
+  onSuccess: () => void,
+  onError: (error: unknown) => void,
+  validateForm: () => boolean
+) => {
+  return (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!validateForm()) {
+      if (onError) onError(new Error("Formulario inválido"));
+      return;
+    }
+    fetch(e.currentTarget.action, {
+      method: e.currentTarget.method,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(Object.fromEntries(new FormData(e.currentTarget))),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          return response.json().then((errorBody) => {
+            let errorMessage = "Error en la respuesta del servidor";
+            if (errorBody.errors) {
+              const errorKeys = Object.keys(errorBody.errors);
+              if (errorKeys.length > 0) {
+                const firstErrorMessage = errorBody.errors[errorKeys[0]];
+                if (firstErrorMessage.length > 0) {
+                  errorMessage = firstErrorMessage[0];
+                }
+              }
+            } else if (errorBody.title) {
+              errorMessage = errorBody.title;
+            } else if (errorBody.message) {
+              errorMessage = errorBody.message;
+            }
+            throw new Error(errorMessage);
+          });
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Success:", data);
+        if (onSuccess) onSuccess();
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        if (onError) onError(error);
+      });
+  };
+};
+
+export default onSubmit;
