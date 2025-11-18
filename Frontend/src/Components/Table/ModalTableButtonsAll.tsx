9e6701a90f9c175b1606fad14f3ded7d58269a50
Máximo quiroga <123843504+MaxIIIDev@users.noreply.github.com>
2025-11-18 02:14:41 -0300

    Update ModalTableButtonsAll.tsx
    elimine comentarios

--- Frontend/src/Components/Table/ModalTableButtonsAll.tsx
+++ Frontend/src/Components/Table/ModalTableButtonsAll.tsx
@@ -6,13 +6,11 @@ interface ButtonEditProps {
 }
 
 export function ButtonEdit({ endpoint }: ButtonEditProps) {
-  // 1. Llama al hook en el nivel superior del componente para obtener la función de navegación.
   const handleEdit = useModalTableEditHandler();
   console.log();
 
   return (
     <>
-      {/* 2. Usa la función obtenida en el evento onClick. */}
       <button onClick={() => handleEdit(endpoint)}>
         <i className="bi bi-pencil"></i> Edit
       </button>
